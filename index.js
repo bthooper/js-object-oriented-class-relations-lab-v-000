76503dbea32178e671e8a51df497673c4ae606d5
Bryan Hooper <bryan@lindsayfarm.net>
2018-11-29 10:45:05 -0600

    Done.

---
+++ index.js
@@ -0,0 +1,93 @@
+idCount = 0;
+passId = 0;
+tripId = 0;
+store = {drivers: [], passengers: [], trips: []};
+
+class Driver {
+  constructor(name) {
+    this.name = name;
+    this.id = ++idCount;
+    store.drivers.push(this);
+  }
+
+  trips() {
+    return store.trips.filter(
+      function(trip) {
+        return trip.driverId === this.id;
+      }.bind(this)
+    );
+  }
+
+  passengers() {
+    return store.passengers.filter(
+      function(passenger) {
+        return store.trips.find(
+          function(trip) {
+            return trip.driverId === this.id;
+          }.bind(this)
+        );
+      }.bind(this)
+    );
+  }
+
+}
+
+
+class Passenger {
+  constructor(name) {
+    this.name = name;
+    this.id = ++passId;
+    store.passengers.push(this);
+  }
+
+  trips() {
+    return store.trips.filter(
+      function(trip) {
+        return trip.passengerId === this.id;
+      }.bind(this)
+    );
+  }
+
+  drivers() {
+    return store.drivers.filter(
+      function(driver) {
+        return store.trips.find(
+          function(trip) {
+            return trip.passengerId === this.id;
+          }.bind(this)
+        );
+      }.bind(this)
+    );
+  }
+}
+
+class Trip {
+  constructor(driver, passenger) {
+    this.id = ++tripId;
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    store.trips.push(this);
+  }
+
+  driver() {
+    return store.drivers.find(
+      function(driver) {
+        return driver.id === this.driverId;
+      }.bind(this)
+    );
+  }
+
+  passenger() {
+    return store.passengers.find(
+      function(passenger) {
+        return passenger.id === this.passengerId;
+      }.bind(this)
+    );
+  }
+}
+
+  
+
+
+
+  
